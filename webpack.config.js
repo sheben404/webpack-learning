b8d22d06ff50767c65880ad9f9ba65a90c15d100
sheben404 <sheben404@gmail.com>
2021-09-11 23:54:39 +0800

    chore: 完成 webpack 打包的最低配置
    现在我们可以运行 yarn build 来打包文件了

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  // 配置模块的入口，可抽象成输入
+  // webpack 执行构建的第一步将从入口开始搜寻及递归解析出所有入口依赖的模块
+  // entry 配置是必填的，如果不填则将导致 webpack 报错退出
+  // 这里我们将 src/index.js 作为入口点
+  entry: {
+    main: path.resolve(__dirname, './src/index.js')
+  },
+
+  // 配置 output 选项可以控制 webpack 如何向硬盘写入编译后文件
+  // 即使可以存在多个入口起点，但是只能制定一个「输出」配置。
+  // 这里我们制定输出配置为 dist
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js'
+  }
+}
